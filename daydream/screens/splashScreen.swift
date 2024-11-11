900d877891ebd7617a14c9ca2a10507a04298a6e
vladislvk <vladislavkim566@gmail.com>
2024-11-11 23:12:56 +0500

    latest

---
+++ daydream/screens/splashScreen.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct splashScreen: View {
+    var body: some View {
+        ZStack {
+            Color.indigo
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    splashScreen()
+}
