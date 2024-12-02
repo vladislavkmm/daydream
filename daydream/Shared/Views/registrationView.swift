3cfe7c323e4b881d3026afb9b3588ce3cd2c6201
vladislvk <vladislavkim566@gmail.com>
2024-12-02 22:03:17 +0500

    feat:implementation of the registration page using local data

--- daydream/Shared/Views/registrationView.swift
+++ daydream/Shared/Views/registrationView.swift
@@ -4,21 +4,33 @@ struct registrationView: View {
     @AppStorage("isRegistered") private var isRegistered: Bool = false
     @State private var userLogin: String = ""
     @State private var userPassword: String = ""
+    @State private var loginError:String? = nil
     var body: some View {
         VStack {
             TextField("enter your login", text: $userLogin)
+                .autocapitalization(.none)
             TextField("enter your password", text: $userPassword)
+                .autocapitalization(.none)
             Button {
                 loginValidation()
             }label: {
                 Text("Login")
             }
-
+            if let error = loginError{
+                Text("error")
+            }
         }
+        
+        
     }
     
     func loginValidation(){
-        isRegistered = true
+        if let _ = users.first(where: {$0.login == userLogin && $0.password == userPassword}){
+            isRegistered = true
+            loginError = nil
+        }else{
+            let loginError = "there is a login error in your login information"
+        }
     }
 }
 
--- daydream/daydreamApp.swift
+++ daydream/daydreamApp.swift
@@ -2,7 +2,6 @@ import SwiftUI
 
 @main
 struct daydreamApp: App {
-    @AppStorage("isRegistered") private var isRegistered: Bool = false
     var body: some Scene {
         WindowGroup {
             if isRegistered {
