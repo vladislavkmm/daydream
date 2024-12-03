1b8675231f969a614cca3e387ef8083f777ab49c
vladislvk <vladislavkim566@gmail.com>
2024-12-03 19:42:50 +0500

    feat: edit registration page

--- daydream/Shared/Views/registrationView.swift
+++ daydream/Shared/Views/registrationView.swift
@@ -6,19 +6,51 @@ struct registrationView: View {
     @State private var userPassword: String = ""
     @State private var loginError:String? = nil
     var body: some View {
-        VStack {
-            TextField("enter your login", text: $userLogin)
-                .autocapitalization(.none)
-            TextField("enter your password", text: $userPassword)
-                .autocapitalization(.none)
-            Button {
-                loginValidation()
-            }label: {
-                Text("Login")
-            }
-            if let error = loginError{
-                Text(error)
-            }
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.purple, Color.white]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea(edges: .top)
+            VStack {
+                
+                TextField("enter your login", text: $userLogin)
+                    .autocapitalization(.none)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.white, lineWidth: 1)
+                        )
+                    .disableAutocorrection(true)
+                TextField("enter your password", text: $userPassword)
+                    .autocapitalization(.none)
+                    .padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.white, lineWidth: 1)
+                        )
+                    .disableAutocorrection(true)
+                if let error = loginError{
+                    Text(error)
+                        .foregroundStyle(Color.purple)
+                        .padding()
+                }
+                Button {
+                    loginValidation()
+                }label: {
+                    Text("Login")
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .padding()
+                        .background(Color.purple)
+                        .cornerRadius(8)
+                        .shadow(radius: 5)
+                                    
+                }
+                
+                    
+            }.padding()
         }
         
         
@@ -29,7 +61,7 @@ struct registrationView: View {
             isRegistered = true
             loginError = nil
         }else{
-            loginError = "there is a login error in your login information"
+            loginError = "There is an issue with the login information provided"
         }
     }
 }
