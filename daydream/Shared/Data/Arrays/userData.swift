3918f1b579430eefaf22ecee615608b7e5d1c8f1
vladislvk <vladislavkim566@gmail.com>
2024-12-09 22:46:41 +0500

    feat: add test data

--- daydream/Shared/Data/Arrays/userData.swift
+++ daydream/Shared/Data/Arrays/userData.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import Foundation
 
 let users: [User] = [
-    User(login: "root@gmail.com", password: "toor", name: "test_name", surname: "test_surname",accountValue: 1000.0,currency: "USD" )
+    User(login: "root@gmail.com", password: "toor", name: "test_name", surname: "test_surname",accountValue: 1000.0,currency: "USD" ),
+    User(login: "root1@gmail.com", password: "toor1", name: "test_name", surname: "test_surname",accountValue: 1000.0,currency: "USD" )
 ]
 
