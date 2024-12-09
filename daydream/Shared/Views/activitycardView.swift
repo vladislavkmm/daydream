38f1d15442bfad6c9fdf3f4235b34ad1d7b7318a
vladislvk <vladislavkim566@gmail.com>
2024-12-09 22:45:19 +0500

    feat: add currency control

--- daydream/Shared/Views/activitycardView.swift
+++ daydream/Shared/Views/activitycardView.swift
@@ -5,12 +5,12 @@ struct activitycardView: View {
         ZStack{
             RoundedRectangle(cornerRadius: 25)
                 .fill(Color.white)
-                .frame(width: .infinity, height: 120)
+                .frame(width: .infinity, height: 100)
             HStack{
                 Text("Food")
                     .font(.title)
                 Spacer()
-                Text("100")
+                Text("100$")
                     .font(.title2)
             }.padding()
         }
