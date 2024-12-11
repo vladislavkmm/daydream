2da8f5b3b7f31bd55ff9e95a67d3eebf41419177
vladislvk <vladislavkim566@gmail.com>
2024-12-11 22:21:38 +0500

    feat: add user activity struct

--- daydream/Shared/Structs/userActivityStruct.swift
+++ daydream/Shared/Structs/userActivityStruct.swift
@@ -1,11 +1,2 @@
 import SwiftUI
-
-struct userActivityStruct: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    userActivityStruct()
-}
+import Foundation
